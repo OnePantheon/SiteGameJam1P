7529c241f8f8ac145c2e31e1f114060266d3bbb1
goldor <dimgoldoritri@gmail.com>
2023-09-22 11:20:51 +0200

    add tailwind css with and a little menu

---
+++ GJ1P/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../app.css";
+</script>
+
+<nav class="w-full bg-red-600 h-[5vh] flex items-center justify-center text-xl drop-shadow-md menu">
+    <a href="/">Home</a>
+</nav>
+
+<slot/>
