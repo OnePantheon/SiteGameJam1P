7b52affacdd0903bf17c450b74ddcfe2c05b54a1
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2023-09-25 19:46:40 +0200

    favicon and currentGameJam
    error to import currentGameJam in page of jamname

--- GJ1P/src/routes/+layout.svelte
+++ GJ1P/src/routes/+layout.svelte
@@ -1,10 +1,23 @@
 <script>
-    import "../app.css";
+    import '../app.css';
+    import { get, writable } from 'svelte/store';
+    export let data;
+
+    // obtenir la page de la première gamejam
+    const currentGameJam = writable(data.gameJams[0]);
+    export function setCurrentGameJam(gamejam) {
+        currentGameJam.set(gamejam);
+    }
+    export function getCurrentGameJam() {
+        return get(currentGameJam);
+    }
+    console.log(getCurrentGameJam());
 </script>
 
 
 <nav class="w-full bg-gradient-to-bl from-red-600 to-red-700 h-[8vh] flex items-center justify-center text-2xl menu drop-shadow-md">
     <a href="/">Home</a>
+    <a href="/gamejam/{data.gameJams[0].name}">GameJam</a>
 </nav>
 
 <div id="content" class="min-h-[92vh]">
---
+++ GJ1P/src/routes/gamejam/[jamname]/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { getCurrentGameJam } from '../../+layout.svelte';
+
+    let currentGameJam = getCurrentGameJam();
+    console.log(currentGameJam);
+
+</script>
+
+<section class="w-full h-full">
+
+    <div class="pt-16 pb-16 pl-20 bg-slate-700">
+
+        <h1 class="text-8xl text-red-600 drop-shadow-md"><strong>
+            {currentGameJam.name}
+        </strong></h1>
+
+        <p class="text-3xl pt-10 w-1/2 text-white drop-shadow-md">BLABLA</p>
+
+    </div>
+
+</section>
