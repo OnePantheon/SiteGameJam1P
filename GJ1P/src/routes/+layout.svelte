a37dd54bd2df539a642f6dec001a4c11df4d8fd8
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2023-10-04 13:14:27 +0200

    some css for login modal

--- GJ1P/src/routes/+layout.svelte
+++ GJ1P/src/routes/+layout.svelte
@@ -6,8 +6,6 @@
     let showModal = false;
     let isLoginSelected = true;
 
-    //console.log(document.getElementById("login").classList.contains("selected"))
-
     function toggleLogin(logreg) {
         isLoginSelected = logreg === "login";
     }
@@ -32,28 +30,39 @@
 -->
 
 <Modal bind:showModal>
-	<h2 slot="header" style="display: flex;">
+	<h2 slot="header" style="display: flex; justify-content: space-around; align-items: center;">
 		<button class:selected={isLoginSelected} on:click={() => toggleLogin("login")} id="login">Login</button>
+        <hr style="height: 20px; border: 1px solid #ccc; margin: 0;">
         <button class:selected={!isLoginSelected} on:click={() => toggleLogin("register")} id="register">Register</button>
 	</h2>
 
-    <div>
+    <div class="modal-content">
         {#if isLoginSelected}
             <form action="/login" method="POST">
+                <div>
                 <label for="username">Username</label>
                 <input type="text" name="username" id="username" required>
+                </div>
+                <div>
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password" required>
+                </div>
                 <button type="submit">Login</button>
             </form>
         {:else}
             <form action="/register" method="POST">
+                <div>
                 <label for="username">Username</label>
                 <input type="text" name="username" id="username" required>
+                </div>
+                <div>
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password" required>
+                </div>
+                <div>
                 <label for="password2">Repeat Password</label>
                 <input type="password" name="password2" id="password2" required>
+                </div>
                 <button type="submit">Register</button>
             </form>
         {/if}
@@ -65,4 +74,21 @@
     .selected {
         font-weight: bold;
     }
+
+    .modal-content > form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .modal-content > form > div {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .modal-content > form > div > input {
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        margin-left: 10px;
+    }
+
 </style>
